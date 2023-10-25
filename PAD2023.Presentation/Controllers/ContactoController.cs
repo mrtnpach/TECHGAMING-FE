33322a33f44d646f066d0c41309bd0702ea9b6c0
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-10-25 17:43:29 -0300

    se realizo el frontend del proyecto

---
+++ PAD2023.Presentation/Controllers/ContactoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PAD2023.Presentation.Controllers
+{
+    public class ContactoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
