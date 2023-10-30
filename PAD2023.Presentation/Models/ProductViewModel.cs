fb33e49ff54a232e57beb44351ccede6d4abdedd
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-10-30 18:36:36 -0300

    se agrego el controlador de productos y su vista

---
+++ PAD2023.Presentation/Models/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PAD2023.Presentation.Models
+{
+    public class ProductViewModel
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public List<string>? ImageUrl { get; set; }
+    }
+}
