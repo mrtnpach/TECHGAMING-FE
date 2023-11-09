cbee4168f48d04e3fa2b521f3c3a949e97d41859
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-11-09 09:46:41 -0300

    se agregaron referencias a la pag 404 de los link no funcionales

--- PAD2023.Presentation/Controllers/HomeController.cs
+++ PAD2023.Presentation/Controllers/HomeController.cs
@@ -35,7 +35,7 @@ namespace PAD2023.Presentation.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View("../Shared/Error");
         }
     }
 }
